229a956945a93fe11b20ae44f2d1055225db56e0
btmxh <ngoduyanh.chip@gmail.com>
2026-01-12 23:45:42 +0900

    feat: add precommit checks

--- flake.nix
+++ flake.nix
@@ -1,36 +1,90 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    systems.url = "github:nix-systems/default";
     flake-utils.url = "github:numtide/flake-utils";
     jailed-agents.url = "github:btmxh/jailed-agents";
+    git-hooks.url = "github:cachix/git-hooks.nix";
   };
 
   outputs =
     {
       self,
       nixpkgs,
+      systems,
       flake-utils,
       jailed-agents,
+      git-hooks,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        formatter = pkgs.nixfmt-tree;
-        devShells.default = pkgs.mkShell {
-          packages =
-            with pkgs;
-            [
-              python3
-              uv
-              nixd
-              nixfmt-rfc-style
-            ]
-            ++ (builtins.attrValues (jailed-agents.lib.${system}.makeJailedAgents { }));
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit (self.checks.${system}.pre-commit-check) shellHook enabledPackages;
+        in
+        {
+          default = pkgs.mkShell {
+            inherit shellHook;
+            buildInputs = enabledPackages;
+            packages =
+              with pkgs;
+              [
+                python3
+                uv
+                ruff
+                ty
+                nixd
+                nixfmt-rfc-style
+              ]
+              ++ (builtins.attrValues (
+                jailed-agents.lib.${system}.makeJailedAgents {
+                  extraPkgs = [
+                    python3
+                    uv
+                    ruff
+                    ty
+                  ];
+                }
+              ));
+          };
+        }
+      );
+
+      # Run the hooks with `nix fmt`.
+      formatter = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit (self.checks.${system}.pre-commit-check) config;
+          inherit (config) package configFile;
+          script = ''
+            ${pkgs.lib.getExe package} run --all-files --config ${configFile}
+          '';
+        in
+        pkgs.writeShellScriptBin "pre-commit-run" script
+      );
+
+      # Run the hooks in a sandbox with `nix flake check`.
+      # Read-only filesystem and no internet access.
+      checks = forEachSystem (system: {
+        pre-commit-check = git-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            nixfmt.enable = true;
+            statix.enable = true;
+            check-yaml.enable = true;
+            end-of-file-fixer.enable = true;
+            trim-trailing-whitespace.enable = true;
+            ruff.enable = true;
+            ruff-format.enable = true;
+            uv-export.enable = true;
+          };
         };
-      }
-    );
+      });
+    };
 }
